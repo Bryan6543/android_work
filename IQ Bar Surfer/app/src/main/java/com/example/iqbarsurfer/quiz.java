4bc4fb9b475db1e7dc624c87fcd83ea7fc7155a2
Bryan6543 <fnirmal802@gmail.com>
2023-06-28 09:39:02 +0530

    s

--- IQ Bar Surfer/app/src/main/java/com/example/iqbarsurfer/quiz.java
+++ IQ Bar Surfer/app/src/main/java/com/example/iqbarsurfer/quiz.java
@@ -2,7 +2,6 @@ package com.example.iqbarsurfer;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -18,10 +17,9 @@ public class quiz extends AppCompatActivity {
     private List<Question> questionList;
     private int currentQuestionIndex;
     private int score;
-    Button  scorebtn;
+//    Button  scorebtn;
     private TextView questionTextView;
     private RadioGroup optionsRadioGroup;
-    private Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +28,20 @@ public class quiz extends AppCompatActivity {
 
         questionTextView = findViewById(R.id.questionTextView);
         optionsRadioGroup = findViewById(R.id.optionsRadioGroup);
-        submitButton = findViewById(R.id.submitButton);
+        Button submitButton = findViewById(R.id.submitButton);
 
         // Prepare the questions
         questionList = new ArrayList<>();
-        questionList.add(new Question("What is the name of the president in Sri lanka", new String[]{"Me", "You", "Your Mama", "Your Dadda"}, 0));
-        questionList.add(new Question("What is wrong with you", new String[]{"NO one knows", "I obviously dont know", "God Might know", "asdkjalsdk"}, 1));
+        questionList.add(new Question("What number comes next in the sequence: 1, 4, 9, 16, 25, ...", new String[]{"28", "32", "38", "36"}, 3));
+        questionList.add(new Question("If all Bloops are Razzies and all Razzies are Lazzies, then which of the following statements must be true?", new String[]{"All Lazzies are Bloops", "All Bloops are Lazzies", "All Razzies are Bloops", "All Lazzies are Razzies"}, 3));
+        questionList.add(new Question("Which of the following words is the odd one out?", new String[]{"Potato", "Tomato", "Carrot", "Apple"}, 3));
+        questionList.add(new Question("A plane crashes on the border of the United States and Canada. Where do they bury the survivors?", new String[]{"United States", "Canada", "Nowhere, you don't bury survivors", "It depends on the nationality of the survivors"}, 2));
+        questionList.add(new Question("If you rearrange the letters \"NOON\", you would get the name of a:", new String[]{"Animal", "City", "Time of day", "Flower"}, 2));
+        questionList.add(new Question("If a cat is worth 10, a dog is worth 15, and a bird is worth 5, what is the value of a horse?", new String[]{"15", "20", "25", "30"}, 2));
+        questionList.add(new Question("How many times can you subtract 10 from 100?", new String[]{"5", "9", "Once", "Infinitely"}, 0));
+        questionList.add(new Question("If a quarter and a dime were in your pocket and you took out a nickel, how much money would you have?", new String[]{"25 cents", "30 cents", "40 cents", "60 cents"}, 3));
+        questionList.add(new Question("Which one of the four is least like the other four?", new String[]{"Apple", "Banana", "Potato", "Grapes"}, 2));
+        questionList.add(new Question("If you rearrange the letters \"EVICTION,\" you would get the name of a:", new String[]{"Country", "Animal", "Profession", "Food"}, 2));
         // Add more questions here...
 
         currentQuestionIndex = 0;
@@ -43,12 +49,7 @@ public class quiz extends AppCompatActivity {
 
         displayQuestion();
 
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                checkAnswer();
-            }
-        });
+        submitButton.setOnClickListener(v -> checkAnswer());
     }
 
     private void displayQuestion() {
